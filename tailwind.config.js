fef51ce9ec03737c61c0e5188d2f8fedd9900eb7
Easy Code <easycode002@gmail.com>
2024-07-02 22:44:19 +0700

    Footer page desogm..

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+      extend: {
+          animation: {
+              marquee: 'marquee 25s linear infinite',
+          },
+          keyframes: {
+              marquee: {
+                  '0%': { transform: 'translateX(0%)' },
+                  '100%': { transform: 'translateX(-100%)' },
+              }
+          },
+      }
   },
   plugins: [],
 }
