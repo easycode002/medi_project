fc63b797390b85c94b3b1cbac5ab2e269e4450fb
Easy Code <easycode002@gmail.com>
2024-07-18 13:37:23 +0700

    Medi project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-      extend: {
-          animation: {
-              marquee: 'marquee 25s linear infinite',
-          },
-          keyframes: {
-              marquee: {
-                  '0%': { transform: 'translateX(0%)' },
-                  '100%': { transform: 'translateX(-100%)' },
-              }
-          },
-      }
-  },
-  plugins: [],
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            animation: {
+                'marquee': 'marquee 5s linear infinite',
+            },
+            keyframes: {
+                marquee: {
+                    '0%': { transform: 'translate3d(var(--move-initial), 0, 0)' },
+                    '100%': { transform: 'translate3d(var(--move-final), 0, 0)' }
+                }
+            }
+        }
+    },
+    plugins: [],
 }
